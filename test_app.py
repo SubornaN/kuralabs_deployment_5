26b3f9ea03baf4eb43933f112c08190c376184e5
suborna <subornadnath@gmail.com>
2022-11-14 17:14:28 -0500

    added test cases for py test

--- test_app.py
+++ test_app.py
@@ -1,10 +1,13 @@
 from application import app, greet
 
-def test_quick():
-  a = "jeff"
-  greeting = greet(a)
-  assert greeting == "Hi jeff "
-
-# def test_home_page():
-#     response = app.test_client().get('/')
-#     assert response.status_code == 200
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    
+def test_invalid_url():
+    response = app.test_client().get('youtube.com')
+    assert response.status_code == 404
+    
+def test_saved_url():
+    response = app.test_client().get('/house')
+    assert response.status_code == 302
